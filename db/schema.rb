2453f1083105ad0527fe59ad6ad466c735837113
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-29 00:21:00 +0800

    Enforce unique emails for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_25_185338) do
+ActiveRecord::Schema.define(version: 2023_05_28_160116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2023_05_25_185338) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "todos", "users"

e95348c91b3e476fa39531342796809e45ec8351
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-29 00:22:03 +0800

    Handle duplicate error gracefully in backend
    Returns a 422 Unprocessable Entity HTTP status code instead of throwing
    an internal server error.
    
    Also reformatted the users constroller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
-    before_action :authorized, only: [:auto_login]
+  before_action :authorized, only: [:auto_login]
 
   # REGISTER
   def create
     @user = User.create(user_params)
     if @user.valid?
-      token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
     else
-      render json: {error: "Invalid email or password"}, status: :unauthorized
+      render json: { error: "Invalid email or password" }, status: :unauthorized
+    end
+  rescue ActiveRecord::RecordNotUnique => e
+    if e.message.include? 'index_users_on_email'
+      render json: { error: "Email already exists" }, status: :unprocessable_entity
+    else
+      render json: { error: e.message }, status: :unprocessable_entity
     end
   end
 
@@ -17,13 +23,12 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
-      token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, token: token }
     else
-      render json: {error: "Invalid email or password"}, status: :unauthorized
+      render json: { error: "Invalid email or password" }, status: :unauthorized
     end
   end
-
 
   def auto_login
     render json: @user
